a09b8db8128629ef75a5c7fa9134abaf1ca3cfd8
Andrew Brunker <andrew@brunker.net.au>
2022-08-17 10:49:49 +1000

    Documentation of the current files

--- dist/lib/helper.d.ts
+++ dist/lib/helper.d.ts
@@ -1,5 +1,32 @@
+/**
+ * Helper utility for resueable functions within TySQL
+ *
+ * @export
+ * @class helper
+ */
 export declare class helper {
+    /**
+     * Creates an instance of helper.
+     *
+     * @memberOf helper
+     */
     constructor();
+    /**
+     * Prints and error message to the console using the supplied information
+     *
+     * @param {string} [message=''] The desired message string
+     * @param {*} [data] Other helpful details, such as a stack trace, or something similar (if applicable)
+     *
+     * @memberOf helper
+     */
     error(message?: string, data?: any): void;
+    /**
+     * Prints a warning message to the console using the supplied information
+     *
+     * @param {string} [message=''] The desired message string
+     * @param {*} [data] Other helpul details, such as a stack trace, applicable variable or something similar (if applicable)
+     *
+     * @memberOf helper
+     */
     warn(message?: string, data?: any): void;
 }
--- dist/lib/helper.js
+++ dist/lib/helper.js
@@ -1,11 +1,38 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helper = void 0;
+/**
+ * Helper utility for resueable functions within TySQL
+ *
+ * @export
+ * @class helper
+ */
 class helper {
+    /**
+     * Creates an instance of helper.
+     *
+     * @memberOf helper
+     */
     constructor() { }
+    /**
+     * Prints and error message to the console using the supplied information
+     *
+     * @param {string} [message=''] The desired message string
+     * @param {*} [data] Other helpful details, such as a stack trace, or something similar (if applicable)
+     *
+     * @memberOf helper
+     */
     error(message = '', data) {
         console.error('🔥ERROR🔥 ::: ' + message, data);
     }
+    /**
+     * Prints a warning message to the console using the supplied information
+     *
+     * @param {string} [message=''] The desired message string
+     * @param {*} [data] Other helpul details, such as a stack trace, applicable variable or something similar (if applicable)
+     *
+     * @memberOf helper
+     */
     warn(message = '', data) {
         console.warn('⚠️WARNING⚠️ ::: ' + message, data);
     }
